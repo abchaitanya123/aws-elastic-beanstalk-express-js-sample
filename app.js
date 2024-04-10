1b31fb445ed6097266d95471ecb564f2bf18126e
abchaitanya123 <cannab@my.bridgeport.edu>
2024-04-10 15:19:46 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to Cloud Project-15'));
+app.get('/', (req, res) => res.send('thank You'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
